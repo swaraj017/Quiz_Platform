044c82de6498e8cad4b39904368d2491990b996b
Swaraj Gaikwad <116364064+swaraj017@users.noreply.github.com>
2025-09-29 20:07:01 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 import cors from "cors";
-app.use(cors({ origin: "http://localhost:5173" }));
+app.use(cors({ origin: "https://quiz-platform-devs.vercel.app/" }));
 
 
 conntectDB();
