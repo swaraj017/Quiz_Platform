216792e56cf440818f88b068922c1b6b3c419394
Swaraj Gaikwad <116364064+swaraj017@users.noreply.github.com>
2025-09-30 14:18:37 +0530

    Update Register.jsx

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -9,7 +9,7 @@ export default function Register() {
   const handleRegister = async (e) => {
     e.preventDefault();
     try {
-      const res = await fetch("http://localhost:5000/register", {
+      const res = await fetch("https://quiz-platform-4gud.onrender.com/register", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ username, email, password }),
