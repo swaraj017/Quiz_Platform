40a5500f849f775755eb35ce498ed61ccb52a167
Swaraj Gaikwad <116364064+swaraj017@users.noreply.github.com>
2025-09-30 14:17:40 +0530

    Update Login.jsx

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -11,7 +11,7 @@ export default function Login() {
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
-      const res = await fetch("http://localhost:5000/login", {
+      const res = await fetch("https://quiz-platform-4gud.onrender.com/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password }),
